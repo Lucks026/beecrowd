bd6ed4fd3e60d7bfe7773cdc4dae031cb0f18afd
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-26 17:17:14 -0300

    fix: corrige beecrowd 1021 - Banknotes and Coins (Java & C++)

--- 1021 - Banknotes and Coins/Main.java
+++ 1021 - Banknotes and Coins/Main.java
@@ -6,12 +6,19 @@ public class Main {
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         double valor = sc.nextDouble();
         int n = (int) Math.round(valor * 100);
-        int[] dens = {10000,5000,2000,1000,500,200,100,50,25,10,5,1};
-        String[] nomes = {"100.00","50.00","20.00","10.00","5.00","2.00","1.00","0.50","0.25","0.10","0.05","0.01"};
-        System.out.printf("%.2f%n", valor);
-        for(int i = 0; i < 12; i++){
-            System.out.println(n / dens[i] + " nota(s) ou moeda(s) de R$ " + nomes[i]);
-            n %= dens[i];
+        int[] notas = {10000, 5000, 2000, 1000, 500, 200};
+        int[] moedas = {100, 50, 25, 10, 5, 1};
+        String[] nNotas = {"100.00","50.00","20.00","10.00","5.00","2.00"};
+        String[] nMoedas = {"1.00","0.50","0.25","0.10","0.05","0.01"};
+        System.out.println("NOTAS:");
+        for(int i = 0; i < 6; i++){
+            System.out.println(n / notas[i] + " nota(s) de R$ " + nNotas[i]);
+            n %= notas[i];
+        }
+        System.out.println("MOEDAS:");
+        for(int i = 0; i < 6; i++){
+            System.out.println(n / moedas[i] + " moeda(s) de R$ " + nMoedas[i]);
+            n %= moedas[i];
         }
     }
 }
--- 1021 - Banknotes and Coins/banknotes_coins.cpp
+++ 1021 - Banknotes and Coins/banknotes_coins.cpp
@@ -4,11 +4,18 @@ int main(){
     double valor;
     cin >> valor;
     int n = round(valor * 100);
-    int dens[] = {10000,5000,2000,1000,500,200,100,50,25,10,5,1};
-    string nomes[] = {"100.00","50.00","20.00","10.00","5.00","2.00","1.00","0.50","0.25","0.10","0.05","0.01"};
-    cout << fixed << setprecision(2) << valor << endl;
-    for(int i = 0; i < 12; i++){
-        cout << n / dens[i] << " nota(s) ou moeda(s) de R$ " << nomes[i] << endl;
-        n %= dens[i];
+    int notas[] = {10000, 5000, 2000, 1000, 500, 200};
+    int moedas[] = {100, 50, 25, 10, 5, 1};
+    string nNotas[] = {"100.00","50.00","20.00","10.00","5.00","2.00"};
+    string nMoedas[] = {"1.00","0.50","0.25","0.10","0.05","0.01"};
+    cout << "NOTAS:" << endl;
+    for(int i = 0; i < 6; i++){
+        cout << n / notas[i] << " nota(s) de R$ " << nNotas[i] << endl;
+        n %= notas[i];
+    }
+    cout << "MOEDAS:" << endl;
+    for(int i = 0; i < 6; i++){
+        cout << n / moedas[i] << " moeda(s) de R$ " << nMoedas[i] << endl;
+        n %= moedas[i];
     }
 }
