2fcb2fd95fc48ba5e73dc9e023d9f80e62f9cfbe
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-26 17:17:15 -0300

    fix: corrige beecrowd 1041 - Coordinates of a Point (Java & C++)

--- 1041 - Coordinates of a Point/Main.java
+++ 1041 - Coordinates of a Point/Main.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt(), y = sc.nextInt();
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        double x = sc.nextDouble(), y = sc.nextDouble();
         if(x == 0 && y == 0) System.out.println("Origem");
-        else if(x == 0 || y == 0) System.out.println("Eixos");
+        else if(y == 0) System.out.println("Eixo X");
+        else if(x == 0) System.out.println("Eixo Y");
         else if(x > 0 && y > 0) System.out.println("Q1");
         else if(x < 0 && y > 0) System.out.println("Q2");
         else if(x < 0 && y < 0) System.out.println("Q3");
