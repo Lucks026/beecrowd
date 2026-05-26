ba712450797d39af8532cd6f49509e21998766ee
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-26 17:20:18 -0300

    feat: add beecrowd 1012 - Area (Python)

---
+++ 1012 - Area/area.py
@@ -0,0 +1,7 @@
+import math
+a = float(input())
+b = float(input())
+c = float(input())
+print("TRIANGULO: {:.3f}".format((a * c) / 2.0))
+print("CIRCULO: {:.3f}".format(math.pi * b * b))
+print("TRAPEZIO: {:.3f}".format(((a + b) * c) / 2.0))
