c5e4ddc0bb5fd1f67adcbc557601c53a379ff674
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-28 17:12:42 -0300

    adiciona solucao Java do exercicio 1080 - Maior e Posicao

---
+++ 1080 - Maior e Posicao/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maior = sc.nextInt();
+        int pos = 1;
+        for(int i = 2; i <= 100; i++){
+            int x = sc.nextInt();
+            if(x > maior){
+                maior = x;
+                pos = i;
+            }
+        }
+        System.out.println(maior);
+        System.out.println(pos);
+    }
+}
