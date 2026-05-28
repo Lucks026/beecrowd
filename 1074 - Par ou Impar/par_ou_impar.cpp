b9b116067ebb5d050c09c5604622b3f120e7ccb6
Lucks026 <lemosbarbosalucas@gmail.com>
2026-05-28 17:09:31 -0300

    adiciona solucao C++ do exercicio 1074 - Par ou Impar

---
+++ 1074 - Par ou Impar/par_ou_impar.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, x;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        if(x == 0){
+            cout << "NULL" << endl;
+        } else {
+            if(x % 2 == 0) cout << "EVEN ";
+            else cout << "ODD ";
+            if(x > 0) cout << "POSITIVE" << endl;
+            else cout << "NEGATIVE" << endl;
+        }
+    }
+}
