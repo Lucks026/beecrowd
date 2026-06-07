25b54bad51e35b7ef4080590454d611ea07391fc
Lucks026 <lemosbarbosalucas@gmail.com>
2026-06-06 23:03:34 -0300

    adiciona solucao C++ do exercicio 1098 - Sequencia IJ 4

---
+++ 1098 - Sequencia IJ 4/sequencia_ij_4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int step = 0; step <= 10; step++){
+        double i = step * 0.2;
+        for(int k = 0; k < 3; k++){
+            double j = 1 + i + k;
+            if(step % 5 == 0){
+                printf("I=%d J=%d\n", (int) round(i), (int) round(j));
+            } else {
+                printf("I=%.1f J=%.1f\n", i, j);
+            }
+        }
+    }
+}
