2a415fd97ddaeac343d3f413d9d646302505cc2a
Lucks026 <lemosbarbosalucas@gmail.com>
2026-06-15 21:40:53 -0300

    adiciona solucao Java do exercicio 1099 - Soma de Impares Consecutivos II

---
+++ 1099 - Soma de Impares Consecutivos II/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int t = 0; t < n; t++){
+            int x = sc.nextInt(), y = sc.nextInt();
+            if(x > y){ int aux = x; x = y; y = aux; }
+            int soma = 0;
+            for(int i = x + 1; i < y; i++){
+                if(i % 2 != 0) soma += i;
+            }
+            System.out.println(soma);
+        }
+    }
+}
