f72105568df53b1e37df58ed15bde68fc5c63e93
Lucks026 <lemosbarbosalucas@gmail.com>
2026-06-15 21:41:03 -0300

    adiciona solucao C++ do exercicio 1113 - Crescente e Decrescente

---
+++ 1113 - Crescente e Decrescente/crescente_e_decrescente.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x, y;
+    while(cin >> x >> y){
+        if(x == y) break;
+        if(x < y) cout << "Crescente" << endl;
+        else cout << "Decrescente" << endl;
+    }
+}
