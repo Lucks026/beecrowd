e09b617597dd75d1523fb5eef8bcca2273af078f
Lucks026 <lemosbarbosalucas@gmail.com>
2026-06-18 02:00:23 -0300

    adiciona solucao Java do exercicio 1133 - Resto da Divisao

---
+++ 1133 - Resto da Divisao/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), y = sc.nextInt();
+        if(x > y){ int aux = x; x = y; y = aux; }
+        for(int i = x + 1; i < y; i++){
+            if(i % 5 == 2 || i % 5 == 3){
+                System.out.println(i);
+            }
+        }
+    }
+}
