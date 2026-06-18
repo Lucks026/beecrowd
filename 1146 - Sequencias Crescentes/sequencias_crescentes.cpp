aac6e21844d3542e77cb45f90ec9c30a97148d19
Lucks026 <lemosbarbosalucas@gmail.com>
2026-06-18 02:00:59 -0300

    adiciona solucao C++ do exercicio 1146 - Sequencias Crescentes

---
+++ 1146 - Sequencias Crescentes/sequencias_crescentes.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    while(cin >> x){
+        if(x == 0) break;
+        for(int i = 1; i <= x; i++){
+            cout << i;
+            if(i < x) cout << " ";
+        }
+        cout << endl;
+    }
+}
