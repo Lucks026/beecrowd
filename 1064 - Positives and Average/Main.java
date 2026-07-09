e3aa16ad32158c29d2334bbedd6d954dcaf3c219
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-08 23:30:22 -0300

    corrige separador decimal com Locale.US no Java do exercicio 1064

--- 1064 - Positives and Average/Main.java
+++ 1064 - Positives and Average/Main.java
@@ -3,6 +3,7 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         int cont = 0;
         double soma = 0.0;
