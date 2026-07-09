e5604ccddf983d365c0fff054043b15c670624fc
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-08 23:30:22 -0300

    corrige separador decimal com Locale.US no Java do exercicio 1036

--- 1036 - Bhaskara's Formula/Main.java
+++ 1036 - Bhaskara's Formula/Main.java
@@ -3,6 +3,7 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         double a = sc.nextDouble(), b = sc.nextDouble(), c = sc.nextDouble();
         double delta = b*b - 4*a*c;
