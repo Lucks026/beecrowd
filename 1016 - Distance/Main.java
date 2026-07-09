0be813ab14cbd04c5ed2561452eb68d1a3763948
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-08 23:30:22 -0300

    corrige separador decimal com Locale.US no Java do exercicio 1016

--- 1016 - Distance/Main.java
+++ 1016 - Distance/Main.java
@@ -3,6 +3,7 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         double v = sc.nextDouble() / 3.6;
         double a = sc.nextDouble();
