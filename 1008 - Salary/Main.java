5b80f47962acc39ac85550ab4bedfd3f19ee8cea
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-08 23:30:22 -0300

    corrige separador decimal com Locale.US no Java do exercicio 1008

--- 1008 - Salary/Main.java
+++ 1008 - Salary/Main.java
@@ -3,6 +3,7 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in).useLocale(Locale.US);
         int number = sc.nextInt();
         int hours = sc.nextInt();
