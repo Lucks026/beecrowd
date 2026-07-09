c58d6f02bbdeeb4268607a92e374c8a1aa68f3c6
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-08 23:30:22 -0300

    corrige separador decimal com Locale.US no Java do exercicio 1038

--- 1038 - Snack/Main.java
+++ 1038 - Snack/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         int cod = sc.nextInt(), qtd = sc.nextInt();
         double[] preco = {0, 4.00, 4.50, 5.00, 2.00, 1.50};
