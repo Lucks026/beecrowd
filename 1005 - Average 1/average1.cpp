43982165ad0851f509e0e741e7be736d3aa2fd95
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-12 22:39:02 -0300

    corrige para media ponderada (pesos 3.5 e 7.5) no C++ do exercicio 1005 - Average 1

--- 1005 - Average 1/average1.cpp
+++ 1005 - Average 1/average1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    double a, b, c, d;
-    cin >> a >> b >> c >> d;
-    printf("MEDIA = %.1f\n", (a + b + c + d) / 4.0);
+    double a, b;
+    cin >> a >> b;
+    printf("MEDIA = %.5f\n", (3.5 * a + 7.5 * b) / 11.0);
 }
