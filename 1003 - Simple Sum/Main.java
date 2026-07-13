d6a262d0702e2e31a00cbe07c4fbd8828529dd28
Lucks026 <lemosbarbosalucas@gmail.com>
2026-07-12 22:39:02 -0300

    corrige saida para inteiro no Java do exercicio 1003 - Simple Sum

--- 1003 - Simple Sum/Main.java
+++ 1003 - Simple Sum/Main.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
-import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        System.out.printf("SOMA = %.1f%n", a + b);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("SOMA = " + (a + b));
     }
 }
