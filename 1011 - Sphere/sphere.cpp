885f2ea5909493be4cfaa89f8c46ff268ffdd5ec
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 17:05:02 -0300

    corrige C++ do exercicio 1011 - Sphere (pi = 3.14159 e 3 casas decimais)

--- 1011 - Sphere/sphere.cpp
+++ 1011 - Sphere/sphere.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main(){
     double r;
     cin >> r;
-    printf("VOLUME = %.4f\n", (4.0 / 3.0) * acos(-1.0) * r * r * r);
+    printf("VOLUME = %.3f\n", (4.0 / 3.0) * 3.14159 * r * r * r);
 }
