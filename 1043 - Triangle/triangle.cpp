c1c07d1209853bdedae4b8084ae4c9028ec80070
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 19:37:26 -0300

    corrige C++ do exercicio 1043 - Triangle (perimetro se valido, area do trapezio se invalido)

--- 1043 - Triangle/triangle.cpp
+++ 1043 - Triangle/triangle.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    double a, b, c; cin >> a >> b >> c;
-    if(a < b+c && b < a+c && c < a+b){
-        cout << "Valido" << endl;
-        if(a==b && b==c) cout << "Equilatero" << endl;
-        else if(a==b || b==c || a==c) cout << "Isosceles" << endl;
-        else cout << "Escaleno" << endl;
-    } else cout << "Invalido" << endl;
+    double a, b, c;
+    cin >> a >> b >> c;
+    if(a < b + c && b < a + c && c < a + b)
+        printf("Perimetro = %.1f\n", a + b + c);
+    else
+        printf("Area = %.1f\n", ((a + b) * c) / 2.0);
 }
