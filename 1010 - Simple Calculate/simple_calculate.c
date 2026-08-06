93af19a7296dd4a2047547d6723ca01470b1f59c
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 17:05:02 -0300

    adiciona solucao C do exercicio 1010 - Simple Calculate

---
+++ 1010 - Simple Calculate/simple_calculate.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int c1, q1, c2, q2;
+    double p1, p2;
+    scanf("%d %d %lf", &c1, &q1, &p1);
+    scanf("%d %d %lf", &c2, &q2, &p2);
+    printf("VALOR A PAGAR: R$ %.2f\n", q1 * p1 + q2 * p2);
+    return 0;
+}
