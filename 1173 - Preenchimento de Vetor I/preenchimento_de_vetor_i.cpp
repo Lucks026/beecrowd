04d6ab219a57325aa557bc5f7d5a35b4d3314470
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 16:28:05 -0300

    adiciona solucao C++ do exercicio 1173 - Preenchimento de Vetor I

---
+++ 1173 - Preenchimento de Vetor I/preenchimento_de_vetor_i.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int v;
+    cin >> v;
+    for(int i = 0; i < 10; i++){
+        cout << "N[" << i << "] = " << v << endl;
+        v *= 2;
+    }
+}
