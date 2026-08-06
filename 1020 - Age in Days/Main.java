ba8c66c7bdc9fa397252031bf8a9ce0cb1500127
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 17:07:37 -0300

    corrige Java do exercicio 1020 - Age in Days (converte dias em anos/meses/dias)

--- 1020 - Age in Days/Main.java
+++ 1020 - Age in Days/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int anos = sc.nextInt(), meses = sc.nextInt(), dias = sc.nextInt();
-        System.out.println(anos * 365 + meses * 30 + dias + " dia(s)");
+        int n = sc.nextInt();
+        System.out.println(n / 365 + " ano(s)");
+        System.out.println((n % 365) / 30 + " mes(es)");
+        System.out.println((n % 365) % 30 + " dia(s)");
     }
 }
