dcbfb63400ea58b57042a02e05abc8975e0bcd67
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 17:07:16 -0300

    corrige Java do exercicio 1016 - Distance (problema correto: tempo = 2X minutos)

--- 1016 - Distance/Main.java
+++ 1016 - Distance/Main.java
@@ -1,13 +1,9 @@
 import java.util.Scanner;
-import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-        double v = sc.nextDouble() / 3.6;
-        double a = sc.nextDouble();
-        double t = sc.nextDouble();
-        System.out.printf("%.3f m%n", v * t + (a * t * t) / 2.0);
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x * 2 + " minutos");
     }
 }
