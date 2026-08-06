7327eee3bc9e8bee5271c217bc15d3aa3280e985
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-06 17:05:02 -0300

    corrige Python do exercicio 1012 - Area (5 areas, raio C e pi = 3.14159)

--- 1012 - Area/area.py
+++ 1012 - Area/area.py
@@ -1,7 +1,8 @@
-import math
-a = float(input())
-b = float(input())
-c = float(input())
+import sys
+
+a, b, c = map(float, sys.stdin.read().split())
 print("TRIANGULO: {:.3f}".format((a * c) / 2.0))
-print("CIRCULO: {:.3f}".format(math.pi * b * b))
+print("CIRCULO: {:.3f}".format(3.14159 * c * c))
 print("TRAPEZIO: {:.3f}".format(((a + b) * c) / 2.0))
+print("QUADRADO: {:.3f}".format(b * b))
+print("RETANGULO: {:.3f}".format(a * b))
