c13fae1574849c0d37db1a07b032b6281e778bfe
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-07 19:51:57 -0300

    adiciona solucao C do exercicio 1043 - Triangle

---
+++ 1043 - Triangle/triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a < b + c && b < a + c && c < a + b)
+        printf("Perimetro = %.1f\n", a + b + c);
+    else
+        printf("Area = %.1f\n", ((a + b) * c) / 2.0);
+    return 0;
+}
