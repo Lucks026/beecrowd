7722c3df5bd2e778ce6189ea663a1a40b945db81
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-07 19:51:57 -0300

    corrige leitura de entrada no Python do exercicio 1040 - Average 3 (quatro notas em uma linha)

--- 1040 - Average 3/average3.py
+++ 1040 - Average 3/average3.py
@@ -1,6 +1,5 @@
-n1, n2 = map(float, input().split())
-n3, n4 = map(float, input().split())
-media = (n1*2 + n2*3 + n3*4 + n4*1) / 10.0
+n1, n2, n3, n4 = map(float, input().split())
+media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10.0
 print("Media: {:.1f}".format(media))
 if media >= 7.0:
     print("Aluno aprovado.")
