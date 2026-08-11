dfee9728d432dd2ec16e26c053c9af50ae156f5c
Lucks026 <lemosbarbosalucas@gmail.com>
2026-08-11 18:37:34 -0300

    adiciona solucao C do exercicio 1071 - Soma de Impares Consecutivos I

---
+++ 1071 - Soma de Impares Consecutivos I/soma_de_impares_consecutivos_i.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    scanf("%d %d", &x, &y);
+    if(x > y){ int t = x; x = y; y = t; }
+    int soma = 0;
+    for(int i = x + 1; i < y; i++)
+        if(i % 2 != 0) soma += i;
+    printf("%d\n", soma);
+    return 0;
+}
